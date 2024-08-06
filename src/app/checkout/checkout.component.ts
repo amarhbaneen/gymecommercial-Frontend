d9e9ca50bc65cf0e40151cabac34c6f2a58e0efb
Amar <amaralzana@gmail.com>
2024-08-06 19:33:01 +0300

    add new function to fetch the year and month from the service to check out package
    add function to handle and show the month depends to the selected year in the checkout form

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {GymCaveFormService} from "../services/gym-cave-form.service";
 
 @Component({
   selector: 'app-checkout',
@@ -10,11 +11,25 @@ export class CheckoutComponent implements OnInit {
   checkoutForm: FormGroup;
   totalQuantity: number = 0;
   totalPrice: number = 0;
+  creditCardMonths:number[] =[];
+  creditCardYears:number[] =[];
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private GymCaveFormService: GymCaveFormService) {
+
   }
 
   ngOnInit(): void {
+    const startMonth = new Date().getMonth() +1 ;
+    this.GymCaveFormService.GetCreditCardMonths(startMonth).subscribe(
+      data =>{
+        this.creditCardMonths = data;
+      }
+    )
+    this.GymCaveFormService.GetCreditCardYears().subscribe(
+      data=>{
+        this.creditCardYears = data;
+      }
+    )
     this.checkoutForm = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: [''],
@@ -35,9 +50,38 @@ export class CheckoutComponent implements OnInit {
         CVC: [''],
       }),
     });
+
+
+
+
   }
 
   onSubmit() {
     console.log(this.checkoutForm.get('customer')?.value);
+  }
+
+  HandleMonthAndYear() {
+
+    const creditCardFormGroup = this.checkoutForm.get('creditCard');
+    const selectedYear :number = Number(creditCardFormGroup?.value.expirationDate);
+    const CurrentYear = new Date().getFullYear();
+    let startMonth = 1
+    if(selectedYear == CurrentYear){
+      startMonth =  new Date().getMonth() +1 ;
+    }
+    else {
+      startMonth = 1
+    }
+
+    this.GymCaveFormService.GetCreditCardMonths(startMonth).subscribe(
+      data =>{
+        this.creditCardMonths = data;
+      }
+    )
+    this.GymCaveFormService.GetCreditCardYears().subscribe(
+      data=>{
+        this.creditCardYears = data;
+      }
+    )
   }
 }
--- src/app/services/gym-cave-form.service.ts
+++ src/app/services/gym-cave-form.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,30 @@ import { Injectable } from '@angular/core';
 export class GymCaveFormService {
 
   constructor() { }
+
+  GetCreditCardYears(): Observable<number[]>{
+    let data: number [] = [];
+    const startYear : number = new Date().getFullYear();
+    const  endYear :number = startYear + 10;
+    for (let theYear = startYear; theYear <= endYear; theYear++){
+      data.push(theYear);
+    }
+
+    return of(data);
+
+
+  }
+
+  GetCreditCardMonths(startMonth: number): Observable<number[]>{
+
+    let data: number [] = [];
+
+    for (let month = startMonth; month <= 12; month++){
+
+      data.push(month);
+    }
+    return of(data);
+
+
+  }
 }
